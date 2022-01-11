c3604874c9500587ec5f44758bfadb5370c708a4
Raphael Mulenda <mulendaraphael@yahoo.fr>
2022-01-11 21:16:58 +0100

    The Bedroom is created by using Java Composition.

---
+++ src/Raphael/Mulenda/Wall.java
@@ -0,0 +1,13 @@
+package Raphael.Mulenda;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
